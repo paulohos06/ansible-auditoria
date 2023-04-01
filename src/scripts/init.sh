316c90c51f0a44f8485d3f68b10aa2d2f331def9
Paulo Henrique <paulo.henrique06@hotmail.com>
2023-04-01 14:56:28 +0000

    update init script and include vagrantfile example

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-main() {
+init_debserver() {
   sudo apt update;
   sudo apt install -y software-properties-common;
   sudo add-apt-repository --yes --update ppa:ansible/ansible;
-  sudo apt install -y git python3.8 ansible wget curl ansible-lint wkhtmltopdf poppler-utils
-  sudo apt install python-is-python3;
-  sudo apt install colorized-logs;
+  sudo apt install -y git python3.8 ansible wget curl ansible-lint;
+  sudo apt install -y python-is-python3;
+  sudo apt install -y aha wkhtmltopdf poppler-utils;
 }
 
-main
+init_rhelserver() {
+  sudo yum update;
+  sudo yum install -y epel-release;
+  sudo yum install -y git wget curl python3.8 ansible ansible-lint;
+  sudo yum install -y python-is-python3;
+  sudo yum install -y aha wkhtmltopdf poppler-utils;
+}
+
+init_debserver
